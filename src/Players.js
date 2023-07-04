b0dea34b1dbcf4e02537f8418c6fa21cb30aeb5f
dhivine4 <igwechibuikedivine@gmail.com>
2023-07-05 00:09:47 +0100

    first commit

---
+++ src/Players.js
@@ -0,0 +1,38 @@
+// creating an array of objects for diff players
+const players = [
+    {
+      name: "Lionel Messi",
+      team: "Inter Maimi",
+      nationality: "Argentina",
+      jerseyNumber: 30,
+      age: 34,
+      imageURL: "https://shorturl.at/uR048",
+    },
+    {
+      name: "Ronaldinho",
+      team: "Inter Milan",
+      nationality: "Brazil",
+      jerseyNumber: 10,
+      age: 42,
+      imageURL: "https://shorturl.at/gklKU",
+    },
+    {
+      name: "Ronaldo",
+      team: "Al-Nassar",
+      nationality: "Portugal",
+      jerseyNumber: 9,
+      age: 45,
+      imageURL: "https://shorturl.at/bAPR5",
+    },
+    {
+      name: "Andrea Pirlo",
+      team: "Juventus",
+      nationality: "Italy",
+      jerseyNumber: 21,
+      age: 42,
+      imageURL: "https://shorturl.at/ijIJZ",
+    }
+  ];
+  
+  
+  export default players;
